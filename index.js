559d5d355a3fae5fd646c4c96c00eb0e5e6a137c
Damian Senn <damian.senn@gmail.com>
2015-05-16 15:21:00 +0200

    Add a second parameter to pass in myth options

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 var Filter = require('broccoli-filter');
 var myth = require('myth');
 
-function MythFilter(inputTree) {
+function MythFilter(inputTree, options) {
 	if (!(this instanceof MythFilter)) {
-		return new MythFilter(inputTree);
+		return new MythFilter(inputTree, options);
 	}
 
 	this.inputTree = inputTree;
+	this.options   = options;
 }
 
 MythFilter.prototype = Object.create(Filter.prototype);
@@ -16,7 +17,7 @@ MythFilter.prototype.constructor = MythFilter;
 MythFilter.prototype.extensions = ['css'];
 MythFilter.prototype.targetExtension = 'css';
 MythFilter.prototype.processString = function (str) {
-	return myth(str);
+	return myth(str, this.options);
 };
 
 module.exports = MythFilter;
